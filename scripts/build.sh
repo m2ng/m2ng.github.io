0da9cbf4b702d51c8c7ed43cd52ccf8a008f77e7
m2ng <30252331+m2ng@users.noreply.github.com>
2022-06-27 10:28:01 +0800

    Path fix after refactor

--- scripts/build.sh
+++ scripts/build.sh
@@ -6,6 +6,6 @@ docker run \
     --rm --entrypoint /bin/sh \
     -v "$PWD/build:/app/build" \
     -v "$PWD/public:/app/public" \
-    -v "$PWD/src:/app/src" m2ng-github-io-react-base \
+    -v "$PWD/src/main:/app/src" m2ng-github-io-react-base \
     -c "npm run build"
 bash ./jekyll.sh build
