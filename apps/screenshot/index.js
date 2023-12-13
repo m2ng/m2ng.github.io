ad5af00cfb8500bc272ae203acd1628fafe2e707
m2ng <30252331+m2ng@users.noreply.github.com>
2023-12-13 18:09:42 +0800

    Add pdf export code

--- apps/screenshot/index.js
+++ apps/screenshot/index.js
@@ -7,9 +7,17 @@ import puppeteer from 'puppeteer';
   const page = await browser.newPage();
   await page.setViewport({ width: 787, height: 562 });
   await page.goto('https://m2ng.github.io', { waitUntil: 'networkidle0' });
+  // await page.goto('http://localhost:3000', { waitUntil: 'networkidle0' });
   await page.screenshot({
     path: 'output/screenshot.jpg',
     quality: 97
   });
+  const pdf = await page.pdf({ 
+    path: 'output/MatthewNg_CV.pdf',
+    format: 'A4',
+    margin: { top: '39px', right: '39px', bottom: '39px', left: '39px' },
+    printBackground: true,
+  });
+
   await browser.close();
 })();
